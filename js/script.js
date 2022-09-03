901e8ec800eca92e2e734a211231671908fcb0cb
Oleg <tsybaoleg@gmail.com>
2022-09-03 12:04:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+"use strict";
+/* let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+   a:50
+};
+
+
+console.log(obj) */
+//alert(10);
+//const result = confirm("are you here?");
+//console.log(result);
+
+//const answer = +prompt("вам есть 18");
+//console.log(answer);
+//console.log(typeof(answer));
+//console.log(answer + 5);
+
+/* const answers = [];
+
+answers [0] = prompt("Как ваше имя", '');
+answers [1] = prompt("Как ваше фамилия", '');
+answers [2] = prompt("Сколько вам лет", '');
+
+console.log(typeof(answers)); */
+
+let incr = 10,
+   decr = 10;
+   //после значения ставится оператор называется постфиксной форма записи
+   //incr++; //опеатор инкримента т.е увеличение на еденицу
+   //decr--;// оператор декриметра т.е уменьшение значения на еденицу
+   // перед значением ставится оператор называется префиксная форма записи
+   //++incr; 
+   //--decr;
+
+   console.log(--incr);
+   console.log(++decr);
+
+   console.log(5%2); //показывает остаток от числа(действия)
+   console.log( 2*4 == 8); //сравнениеи
+   console.log( 2*4 === '8');//  строгое сравнение4
+   console.log(2 + 2 * 2 ===8);
+   console.log(2 + 2 * 2 !=8);//
+   // логические операторы
+   //&&  // оператор означает (и)
+  // || // оператор означает (или)
+
+   const isChecked = true,
+         isClose = false;
+
+   console.log(isChecked || isClose);    
+
